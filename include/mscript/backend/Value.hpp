8d1224943fe53d486df7a1cfe807be06c597c12c
Maks Makuta <maksmakuta@proton.me>
2026-05-02 13:01:47 +0300

    [0.4.0] Value datatype

---
+++ include/mscript/backend/Value.hpp
@@ -0,0 +1,80 @@
+#ifndef MSCRIPT_VALUE_HPP
+#define MSCRIPT_VALUE_HPP
+
+#include <functional>
+#include <map>
+#include <memory>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace ms {
+
+    struct FunctionStmt;
+
+    struct Array;
+    struct Dict;
+    struct Function;
+
+    using Value = std::variant<
+        std::monostate,
+        bool,
+        int64_t,
+        double,
+        std::string,
+        std::shared_ptr<Array>,
+        std::shared_ptr<Dict>,
+        std::shared_ptr<Function>
+    >;
+
+    using Values = std::vector<Value>;
+
+    struct Array {
+        Values values;
+    };
+
+    struct Dict {
+        std::map<Value,Value> values;
+    };
+
+    struct Function {
+        std::function<Value(Values)> native_fn;
+        const FunctionStmt* fn = nullptr;
+
+        bool isNative() const {
+            return !!native_fn;
+        }
+    };
+
+    template<typename T>
+    bool is(const Value& v) {
+        return std::holds_alternative<T>(v);
+    }
+
+    inline std::string typeName(const Value& v) {
+        return std::visit([]<typename T>(T&& val) -> std::string {
+            using Decayed = std::decay_t<T>;
+            if constexpr (std::is_same_v<Decayed, std::monostate>)
+                return "null";
+            else if constexpr (std::is_same_v<Decayed, bool>)
+                return "bool";
+            else if constexpr (std::is_same_v<Decayed, int64_t>)
+                return "int";
+            else if constexpr (std::is_same_v<Decayed, double>)
+                return "real";
+            else if constexpr (std::is_same_v<Decayed, std::string>)
+                return "string";
+            else if constexpr (std::is_same_v<Decayed, std::shared_ptr<Array>>)
+                return "array";
+            else if constexpr (std::is_same_v<Decayed, std::shared_ptr<Dict>>)
+                return "dict";
+            else if constexpr (std::is_same_v<Decayed, std::shared_ptr<Function>>)
+                return val->isNative() ? "native function" : "function";
+            else
+                return "unknown";
+        }, v);
+    }
+
+}
+
+#endif //MSCRIPT_VALUE_HPP
